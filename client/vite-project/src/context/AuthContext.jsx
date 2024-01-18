0d34df4928a3660e16284b6a39e803e9bfadf68e
o.allawa <o.allawa@DESKTOP-EGON3FG>
2024-01-18 13:28:10 +0200

    add login functionality

--- client/vite-project/src/context/AuthContext.jsx
+++ client/vite-project/src/context/AuthContext.jsx
@@ -12,8 +12,14 @@ const [registerInfo , setRegisterInfo] = useState({
     email : "",
     password : ""
 });
-
+const [loginError, setLoginError] = useState(null);
+const [isLoginLoading, setIsLoginLoading] = useState(false);
+const [loginInfo , setLoginInfo] = useState({
+    email : "",
+    password : ""
+});
 console.log("Userr", user);
+console.log("loginInfo", loginInfo);
 
 useEffect(() => {
    const user = localStorage.getItem("User");
@@ -22,10 +28,14 @@ useEffect(() => {
 },[]);
 
 
-console.log("registerInfo", registerInfo);
+// console.log("registerInfo", registerInfo);
 
 const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info);
+}, []);
+
+const updateLoginInfo = useCallback((info) => {
+    setLoginInfo(info);
 }, []);
 
 const registerUser = useCallback(async(e) => {
@@ -44,6 +54,23 @@ const registerUser = useCallback(async(e) => {
    setUser(response)
 }, [registerInfo]);
 
+const loginUser = useCallback(async(e) =>{
+e.preventDefault()
+ setIsLoginLoading(true)
+ setLoginError(null)
+    const response =  await postRequest(
+        `${baseUrl}/users/login` , 
+        JSON.stringify(loginInfo)
+        );
+
+        setIsLoginLoading(false)
+        if(response.error){
+            return setLoginError(response);
+        }
+     localStorage.setItem("User", JSON.stringify(response))
+     setUser(response)
+},[loginInfo])
+
  const logoutUser = useCallback(()=>{
     localStorage.removeItem("User");
     setUser(null);
@@ -58,7 +85,12 @@ const registerUser = useCallback(async(e) => {
         registerUser,
         registerError,
         isRegisterLoading,
-        logoutUser
+        logoutUser,
+        loginUser,
+        loginInfo,
+        updateLoginInfo,
+        loginError,
+        isLoginLoading
     }}>
           {children}
     </AuthContext.Provider>
