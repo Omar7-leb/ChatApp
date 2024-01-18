81d284a4880cc99bb8602497e761a76752dec859
o.allawa <o.allawa@DESKTOP-EGON3FG>
2024-01-18 16:20:22 +0200

    add messages api

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const app = express();
 const userRoute = require('./Routes/userRoute');
 const chatRoute = require('./Routes/chatRoute');
+const messageRoute = require('./Routes/messageRoute');
 require("dotenv").config();
 
 app.use(express.json())
 app.use(cors());
 app.use("/api/users" ,userRoute); 
 app.use("/api/chats" ,chatRoute);
+app.use("/api/messages" ,messageRoute);
 
 app.get('/', (req , res) => {
     res.send("Welcome our chat app APIs..")
